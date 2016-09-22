e0a37a73781dfb783fe0156318f790a0581a08ae
zigorrom <zigorrom@gmail.com>
2016-09-22 19:25:45 +0200

    Ranges

--- py_pulse.py
+++ py_pulse.py
@@ -1,54 +1,127 @@
 import visa
 import time
 
-class Keithley2430:
+class Keithley24XX:
     def __init__(self,resource):
         rm = visa.ResourceManager()
         self.instrument = rm.open_resource(resource)
 
-##    def SwitchOutput(self, output):
-##        if output is bool:
-##            command = 'OFF'
-##            if output:
-##                command = 'ON'     
-##            self.instrument.write("OUTP:STAT {0}".format(command))
-##            
+##################################################################################
+##
+##  SET FUNCTION SHAPE
+##
 
-    def OutputOff(self):
-        self.instrument.write("OUTP:STAT OFF")
-        if self.instrument.ask("OUTP:STAT?") == 'OFF':
-            return True
-        else:
-            return False
-        
-        
-    def StartOutput(self):
-        self.instrument.write(":INIT")
+    FUNCTION_SHAPES = ['DC', 'PULS']
+    DC_SHAPE,PULSE_SHAPE=FUNCTION_SHAPES
+
+    def SetFunctionShape(self,shape):
+        if shape in self.FUNCTION_SHAPES:
+            self.instrument.write("SOUR:FUNC:SHAP {0}".format(shape))
     
-    def OutputOn(self):
-        self.instrument.write("OUTP:STAT ON")
-        if self.instrument.ask("OUTP:STAT?") == 'ON':
-            return True
-        else:
-            return False
-
-    def StartPulseAndRead(self):
-        return self.instrument.ask(":READ?")
-
     def SetDC(self):
-        self.instrument.write("SOUR:FUNC:SHAP DC")
+        self.SetFunctionShape(self.DC_SHAPE)
 
     def SetPulse(self):
-        self.instrument.write("SOUR:FUNC:SHAP PULS")
+        self.SetFunctionShape(self.PULSE_SHAPE)
 
-    def SetVoltageMode(self):
-        self.instrument.write("SOUR:FUNC VOLT")
+##
+##  END SET FUNCTION SHAPE
+##
+##################################################################################
+##
+##  SET SOURCE FUNCTION
+##
 
-    def SetCurrentMode(self):
-        self.instrument.write("SOUR:FUNC CURR")
+    SOURCE_FUNCTIONS = ['VOLT','CURR']
+    VOLT_SOURCE_FUNCTION, CURR_SOURCE_FUNCTION = SOURCE_FUNCTIONS
 
-    def SetVoltageAmplitude(self,voltage):
-        self.instrument.write("SOUR:VOLT:LEV:IMM:AMPL {0}".format(voltage))
+    def SetSourceFunction(self,func):
+        if func in self.SOURCE_FUNCTIONS:
+            self.instrument.write("SOUR:FUNC {0}".format(func))
+    
+    def SetVoltageSourceFunction(self):
+        self.SetSourceFunction(self.VOLT_SOURCE_FUNCTION)
+    
+
+    def SetCurrentSourceFunction(self):
+        self.SetSourceFunction(self.CURR_SOURCE_FUNCTION)
+
+##
+##  END SET SOURCE FUNCTION
+##
+##################################################################################
+##
+##  SET SOURCING MODE
+##
+    SOURSING_MODES = ['FIX','LIST','SWE']
+    FIXED_SOURCING_MODE, lIST_SOURCING_MODE,SWEEP_SOURCING_MODE = SOURSING_MODES
+
+    def SetSourceMode(self,func, mode):
+        if (mode in self.SOURSING_MODES) and (func in self.SOURCE_FUNCTIONS):
+            self.instrument.write("SOUR:{f}:MODE {m}".format(f=func,m=mode))
+
+    def SetFixedVoltageSourceMode(self):
+        self.SetSourcingMode(VOLT_SOURCE_FUNCTION,FIXED_SOURCING_MODE)
+
+    def SetFixedCurrentSourceMode(self):
+        self.SetSourcingMode(CURR_SOURCE_FUNCTION,FIXED_SOURCING_MODE)
+
+    def SetListVoltageSourceMode(self):
+        self.SetSourcingMode(VOLT_SOURCE_FUNCTION,lIST_SOURCING_MODE)
+
+    def SetLisrCurrentSourceMode(self):
+        self.SetSourcingMode(CURR_SOURCE_FUNCTION,lIST_SOURCING_MODE)
+        
+    def SetSweepVoltageSourceMode(self):
+        self.SetSourcingMode(VOLT_SOURCE_FUNCTION,SWEEP_SOURCING_MODE)
+        
+    def SetSweepCurrentSourceMode(self):
+        self.SetSourcingMode(CURR_SOURCE_FUNCTION,SWEEP_SOURCING_MODE)
+
+        
+##
+##  END SET SOURCING MODE
+##
+##################################################################################
+##
+##  SET SOURCING RANGE
+##
+    DEFAULT_RANGES = ['DEF','MIN','MAX','UP','DOWN']
+    DEFAULT_RANGE,MIN_RANGE,MAX_RANGE,UP_RANGE,DOWN_RANGE = DEFAULT_RANGES
+
+    AUTO_RANGE_STATES = AUTO_RANGE_ON, AUTO_RANGE_OFF = ['ON','OFF']
+    
+    ALL_VOLTAGE_RANGES = ['200E-3','2','20','100']
+    VOLT_RANGE_200mV,VOLT_RANGE_2V,VOLT_RANGE_20V,VOLT_RANGE_100V = ALL_VOLTAGE_RANGES
+
+    ALL_CURRENT_RANGES = ['10E-6','100E-6','1E-3','10E-3','100E-3','1']
+    CURR_RANGE_10uA,CURR_RANGE_100uA,CURR_RANGE_1mA,CURR_RANGE_10mA,CURR_RANGE_100mA,CURR_RANGE_1A = ALL_CURRENT_RANGES
+
+    def SetSourceRange(self,func,range):
+        if func in self.SOURCE_FUNCTIONS:
+            if(range in self.DEFAULT_RANGES) or (range in self.ALL_VOLTAGE_RANGES) or (range in self.ALL_CURRENT_RANGES):
+                self.instrument.write("SOUR:{f}:RANG {r}".format(f=func,r=range))
+
+    def SetVoltageSourceRange(self,range):
+        if range in self.ALL_VOLTAGE_RANGES:
+            self.SetSourceRange(self.VOLT_SOURCE_FUNCTION,range)
+        
+    def SetCurrentSourceRange(self,range):
+        if range in self.ALL_CURRENT_RANGES:
+            self.SetSourceRange(self.CURR_SOURCE_FUNCTION,range)
+
+    def SetAutoRange(self,func, state):
+        if func in self.SOURCE_FUNCTIONS:
+            if state in self.AUTO_RANGE_STATES:
+                self.instrument.write("SOUR:{f}:RANG:AUTO {s}".format(f = func,s = state))
+
+##
+##  END SET SOURCING RANGE
+##  
+
+
+
+        
 
     MIN_PULSE_WIDTH = 0.00015
     MAX_PULSE_WIDTH = 0.005
@@ -68,6 +141,53 @@ class Keithley2430:
             delay = self.MAX_DELAY
         self.instrument.write("SOUR:PULS:WIDT {0}".format(delay))
 
+
+
+
+    
+    ## implement fixed sourcing mode
+
+    ## implement range
+
+        
+    def SetVoltageAmplitude(self,voltage):
+        self.instrument.write("SOUR:VOLT:LEV {0}".format(voltage))
+
+
+    def SetCurrentAmplitude(self,current):
+        self.instrument.write("SOUR:CURR:LEV {0}".format(current))
+
+    
+        
+    def OutputOff(self):
+        self.instrument.write("OUTP:STAT OFF")
+        if self.instrument.ask("OUTP:STAT?") == 'OFF':
+            return True
+        else:
+            return False
+        
+        
+    def StartOutput(self):
+        self.instrument.write(":INIT")
+    
+    def OutputOn(self):
+        self.instrument.write("OUTP:STAT ON")
+        if self.instrument.ask("OUTP:STAT?") == 'ON':
+            return True
+        else:
+            return False
+
+    def StartOutputAndRead(self):
+        return self.instrument.ask(":READ?")
+
+    
+
+    
+
+    
+    
+    
+    
     def DisablePulseMeasurements(self):
         self.instrument.write(":SENSe:FUNCtion:OFF:ALL")
 
@@ -89,24 +209,31 @@ class Keithley2430:
     
 
 if __name__ == "__main__":
-    k = Keithley2430('GPIB0::5::INSTR')
+    k = Keithley24XX('GPIB0::5::INSTR')
 ##    k.Reset()
     print(k.IDN())
-    k.SetCurrentMode()
+    
+    k.SetCurrentSourceFunction()
     time.sleep(1)
-    k.SetVoltageMode()
+    k.SetVoltageSourceFunction()
+    
     time.sleep(1)
     k.SetPulse()
     k.DisablePulseMeasurements()
     k.SetPulseWidth(0.005)
     k.SetTriggerCount(1)
-##    k.SetPulseDelay(0.01)
+
     time.sleep(1)
-    k.SetVoltageAmplitude(1)
+    k.SetVoltageSourceRange(k.MIN_RANGE)
     time.sleep(1)
-####    k.StartPulseAndRead()
-####    k.StartOutput()
+    k.SetAutoRange(k.VOLT_SOURCE_FUNCTION,k.AUTO_RANGE_ON)
+    print(k.AUTO_RANGE_ON)
+##    k.SetSourcingRange(k.VOLT_SOURCE_FUNCTION,k.VOLT_RANGE_200mV)
+    k.SetVoltageAmplitude(100)
+    time.sleep(1)
+##    k.StartOutput()
     k.OutputOn()
+##    print(k.StartOutputAndRead())
     time.sleep(2)
     k.OutputOff()
     k.SetDC()
